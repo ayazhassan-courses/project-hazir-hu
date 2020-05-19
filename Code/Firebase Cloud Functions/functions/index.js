17cd48fc3d58c355162fbb69cf5923e638374236
Faaz Abidi <sa06195@st.habib.edu.pk>
2020-05-20 01:20:31 +0500

    update functions

--- Code/Firebase Cloud Functions/functions/index.js
+++ Code/Firebase Cloud Functions/functions/index.js
@@ -1,10 +1,18 @@
 const functions = require('firebase-functions');
 const fetch = require("node-fetch");
 const admin = require('firebase-admin');
+const runtimeOpts = {
+    timeoutSeconds: 540,
+    memory: '512MB'
+}
 
 admin.initializeApp();
 let db = admin.firestore();
 
+
+// this function will be used to get new user data, it returns json response
+// if user's data has been added to database : { status : "data updated"  }
+// if invalid credentials : { status : 'invalid credentials' }
 exports.getData = functions.https.onRequest(async(request, response) => {
 
     const huid = request.query.huid;
@@ -27,23 +35,35 @@ exports.getData = functions.https.onRequest(async(request, response) => {
             .then(apiData => apiData.json())
             .then(apiData => {
                 admin.database().ref(`/users/${userKey}`).set(apiData);
-                response.send('Success')
+                db.collection('users').doc(huid).update({ lastupdated: apiData.last_updated })
+                response.send({
+                    status: 'data updated'
+                })
             });
     } catch (err) {
-        response.send('Error')
+        response.send({
+            status: 'invalid credentials'
+        })
     }
 });
 
 
+// this is a login function
+// response: if user exists: { status : ''user already exists'' }
+// if user does not exists: {status : 'user added', name = 'user name' }
+// if new user fails login check : {status : 'invalid credentials' }
+// if network error : { status : 'network error }
+// if anyother error : { status: 'something is wrong'}
 
+exports.login = functions.https.onRequest(async(request, response) => {
 
-exports.login = functions.https.onRequest(async(req, res) => {
-
-    const huid = req.query.huid;
-    const pass = req.query.pass;
-    const token = req.query.token;
+    const huid = request.query.huid;
+    const pass = request.query.pass;
+    const token = request.query.token;
     let userExists = false;
-    const data = {
+    const logincheckUrl = `https://hazirapi.herokuapp.com/logincheck?id=${huid}&pwd=${pass}`
+    let loginCheck = false;
+    const userdata = {
         huid: huid,
         pass: pass,
         token: token
@@ -53,7 +73,7 @@ exports.login = functions.https.onRequest(async(req, res) => {
     let userDoc = await UserRef.get()
         .then(snapshot => {
             snapshot.forEach(doc => {
-                if (doc.id == data.huid) {
+                if (doc.id == userdata.huid) {
                     userExists = true;
                 }
             });
@@ -61,14 +81,98 @@ exports.login = functions.https.onRequest(async(req, res) => {
 
 
     if (userExists === false) {
-        var newUserReq = admin.database().ref('/users').push(data);
-        var userUniqueKey = newUserReq.key;
-        data['key'] = userUniqueKey
-        db.collection('users').doc(data.huid).set(data)
-        res.send('User Added')
+
+        try {
+            await fetch(logincheckUrl)
+                .then(apiData => apiData.json())
+                .then(apiData => {
+                    if (apiData.status == 'success') {
+                        loginCheck = true
+                        let userName = apiData.name
+                    } else if (apiData.status == 'invalid credentials') {
+                        response.send({
+                            status: 'invalid credentials'
+                        })
+                    }
+                });
+        } catch (err) {
+            response.send({
+                status: 'network error'
+            })
+        }
+
+        if (loginCheck == true) {
+            var newUserReq = admin.database().ref('/users').push(userdata);
+            var userUniqueKey = newUserReq.key;
+            userdata['key'] = userUniqueKey
+            db.collection('users').doc(userdata.huid).set(userdata)
+            response.send({
+                status: 'user added',
+                name: userName
+            })
+        } else {
+            response.send({
+                status: 'something is wrong'
+            })
+        }
     } else {
-        res.send('User Already Exists')
+        response.send({
+            status: 'user already exists'
+        })
     }
+});
+
+// schedule function
+// this is not a callable function
+// this will update all the user's data that is outdated for more than 3 hours
+// error handling responses can be found in function logs.
+exports.scheduledFunction = functions.runWith(runtimeOpts).pubsub.schedule('every 20 minutes').onRun(async(context) => {
+
+
+    let usertoUpdate = [];
+    const currentTime = Date.now()
+
+    let UserRef = db.collection('users');
+    let userDoc = await UserRef.get()
+        .then(snapshot => {
+            snapshot.forEach(doc => {
+                if (usertoUpdate.length < 26) {
+                    usertoUpdate.push(doc.data())
+                }
+            });
+        })
+
+    console.log(usertoUpdate)
+
+    if (usertoUpdate.length > 0) {
+        for (const eachUser of usertoUpdate) {
+            const userlastupdatedTime = eachUser.lastupdated
+            const timeDifference = currentTime - userlastupdatedTime
+            const huid = eachUser.huid
+            const pass = eachUser.pass
+            const userKey = eachUser.key
+            const url = `https://hazirapi.herokuapp.com/login?id=${huid}&pwd=${pass}`
+
+            if (timeDifference > 180) {
+                try {
+                    await fetch(url)
+                        .then(apiData => apiData.json())
+                        .then(apiData => {
+                            admin.database().ref(`/users/${userKey}`).set(apiData);
+                            db.collection('users').doc(huid).update({ lastupdated: apiData.last_updated })
+                            console.log(`Data updated for user: ${huid}`)
+                        });
+                } catch (err) {
+                    console.log(`Failed to update ${huid}`)
+                }
+            }
+        }
+
+    } else {
+        console.log('All users are upto date')
+    }
+
+
 });
 
 exports.generateNotifications = functions.https.onRequest((request, response) => {
@@ -78,7 +182,7 @@ exports.generateNotifications = functions.https.onRequest((request, response) =>
     const type = request.query.type;
     const coursename = request.query.coursename;
     const date = request.query.date;
- 
+
     if (type == 'absent') {
         var message = {
             notification: {
@@ -114,6 +218,3 @@ exports.generateNotifications = functions.https.onRequest((request, response) =>
 
 
 });
-
-
-
